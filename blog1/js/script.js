4251955ab43b4361ab4420592bad38dd135a87c7
phos <krabbphos@gmail.com>
2016-08-15 10:26:23 +0700

    template with screen shot

---
+++ blog1/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$(".myclick").click(function(){
+		// $(".myfrm").css("display","block");
+		$(".myfrm").toggle(200);
+	});
+});
---
+++ blog6/js/script.js
@@ -0,0 +1,7 @@
+
+jQuery(document).ready(function() {
+	jQuery('.post').addClass("hidden").viewportChecker({
+	    classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+	    offset: 100    
+	   });   
+});            
