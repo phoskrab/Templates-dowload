1ce886a6ed228d82002b255f22f92ebe849b9f3d
phos <krabbphos@gmail.com>
2016-08-23 17:25:25 +0700

    two template added

---
+++ business/import_company/js/script.js
@@ -0,0 +1,8 @@
+/*script smooth scroll*/
+	//script smooth*/
+	jQuery(document).ready(function() {
+	jQuery('.no').addClass("hidden").viewportChecker({
+	    classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+	    offset: 100    
+	   });   
+	}); 
